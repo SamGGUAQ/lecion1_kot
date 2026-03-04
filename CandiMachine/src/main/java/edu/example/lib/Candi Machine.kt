e1b7551ce0e588c61a02612c93bb06e5ebcdce85
SamGGUAQ <sgarcia79@alumnos.uaq.mx>
2026-03-04 08:50:34 -0600

    is already working

--- CandiMachine/src/main/java/edu/example/lib/Candi Machine.kt
+++ CandiMachine/src/main/java/edu/example/lib/Candi Machine.kt
@@ -1,6 +1,6 @@
 package edu.example.lib
 class CashRegister(cashIn: Int = 500) {
-    var cashOnHand = if (cashIn >= 0) cashIn else 500 // para defaul? o no se puede tener numeros negativos?
+    var cashOnHand = if (cashIn >= 0) cashIn else 500 // ddefault o no negativos?
 
     fun getBalance(): Int {
         return cashOnHand
@@ -13,47 +13,22 @@ class CashRegister(cashIn: Int = 500) {
     }
 }
 class Dispenser(setNoOfItems: Int = 50, setCost: Int = 50) {
-    var numberOfItems: Int = if (setNoOfItems >= 0) setNoOfItems else 50 // supongo que no negativos igual
+    var numberOfItems: Int = if (setNoOfItems >= 0) setNoOfItems else 50
     var cost: Int = if (setCost >= 0) setCost else 50
 
     fun getCount(): Int = numberOfItems
     fun getProductCost(): Int = cost
     fun makeSale() {
-            numberOfItems-- // [cite: 192]
-    }
-}
-
-fun main() {
-    val cashRegister = CashRegister()
-    val candy = Dispenser(100, 50)
-    val chips = Dispenser(100, 65)
-    val gum = Dispenser(75, 45)
-    val cookies = Dispenser(100, 85)
-    var choice: Int = 0
-    while (choice != 9) {
-        showSelection()
-
-        // Leemos la entrada del usuario dentro del bucle
-        val input = readln()
-        choice = input.toIntOrNull() ?: 0 // Si no es un número, ponemos 0
-
-        when (choice) {
-            1 -> sellProduct(candy, cashRegister)
-            2 -> sellProduct(chips, cashRegister)
-            3 -> sellProduct(gum, cashRegister)
-            4 -> sellProduct(cookies, cashRegister)
-            9 -> println("------------------")
-            else -> println("Invalid selection")
-        }
+            numberOfItems--
     }
 }
 fun showSelection() {
-    println("\n*** Welcome to Shelly´s Candy Bar ***")
-    println("to select an item, enter")
-    println("1 for Candy")
-    println("2 for Chips")
-    println("3 for Gum")
-    println("4 for Cookies")
+    println("\nwelcome to shelly´s candy bar")
+    println("to select an item enter")
+    println("1 for candy")
+    println("2 for chips")
+    println("3 for gum")
+    println("4 for cookies")
     println("9 to exit")
     print("select: ")
 }
@@ -63,16 +38,40 @@ fun sellProduct(product: Dispenser, cRegister: CashRegister) {
         var coinsInserted = 0
 
         while (coinsInserted < price) {
-            val missing = price - coinsInserted
-            print("Please deposit  $missing cents ")
+            val mising = price - coinsInserted
+            print("Please deposit  $mising cents ")
 
             val deposit = readln().toIntOrNull() ?: 0
             coinsInserted += deposit
         }
         cRegister.acceptAmount(coinsInserted)
         product.makeSale()
-        println("Collect your item at the bottom and enjoy")
+        println("collect your item at the bottom and enjoy")
     } else {
-        println("Sorry this item is sold out")
+        println("sorry this item is sold out")
+    }
+}
+
+fun main() {
+    val cashRegister = CashRegister()
+    val candy = Dispenser(100, 50)
+    val chips = Dispenser(100, 65)
+    val gum = Dispenser(75, 45)
+    val cookies = Dispenser(100, 85)
+    var choice = 0
+    while (choice != 9) {
+        showSelection()
+
+        val input = readln()
+        choice = input.toIntOrNull() ?: 0
+
+        when (choice) {
+            1 -> sellProduct(candy, cashRegister)
+            2 -> sellProduct(chips, cashRegister)
+            3 -> sellProduct(gum, cashRegister)
+            4 -> sellProduct(cookies, cashRegister)
+            9 -> println("BYE")
+            else -> println("invalid selection")
+        }
     }
 }
